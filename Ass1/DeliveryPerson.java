bb6e66c8b7e28adb54a24d0291b7f1806a52c422
CiTRuS93 <hadarradah@gmail.com>
2019-03-20 16:56:50 +0200

    Add files via upload

---
+++ Ass1/DeliveryPerson.java
@@ -0,0 +1,13 @@
+public class DeliveryPerson extends People {
+
+	/**
+	 * 
+	 * @param flowers
+	 * @param people
+	 */
+	public void deliver(FlowersBouquet flowers, People people) {
+		// TODO - implement DeliveryPerson.deliver
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Ass1/Gardner.java
@@ -0,0 +1,12 @@
+public class Gardner extends People {
+
+	/**
+	 * 
+	 * @param flowers
+	 */
+	public FlowersBouquet processOrder(String flowers) {
+		// TODO - implement Gardner.processOrder
+		throw new UnsupportedOperationException();
+	}
+
+}
