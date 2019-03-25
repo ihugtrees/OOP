e1765095df8145f434fdffaf330b6c4f104fd5c9
iHugTrees <igoralikin@gmail.com>
2019-03-25 19:53:36 +0200

    fixed letters

--- Ass1/src/Main.java
+++ Ass1/src/Main.java
@@ -2,19 +2,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        Person chris = new Person("Chris");
-        Florist fred = new Florist("fred", null, null, null);
-        chris.setFlorist(fred);
 
+        Person chris = new Person("Chris");
+        Florist fred = new Florist("Fred", null, null, null);
         DeliveryPerson delivery = new DeliveryPerson("Delivery Person");
         Arranger arranger = new Arranger("Flower Arranger");
         Gardner gardener = new Gardner("Gardner");
-        Grower grower = new Grower("grower", gardener);
+        Grower grower = new Grower("Grower", gardener);
         Wholesaler saler = new Wholesaler("Wholesaler", grower);
-
         Person robin = new Person("Robin");
-
         Florist robins = new Florist("Robin’s Florist", delivery, arranger, saler);
+
+        chris.setFlorist(fred);
         robin.setFlorist(robins);
 
         chris.sendFlowers("Roses, Violets, Gladiolus",robin);
