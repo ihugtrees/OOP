5001b7a37e45f8d01036dd2e765eae6d64ed0fb6
iHugTrees <igoralikin@gmail.com>
2019-06-12 20:11:53 +0300

    test

---
+++ Ass3/Tests/ActiveGameUnitTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActiveGameUnitTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void defend() {
+    }
+}
